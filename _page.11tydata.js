63d25204fab8ef1270ce63933ce3c830bb5b7e2d
Anton Staroverov <73071319+tonystar@users.noreply.github.com>
2026-05-10 14:01:56 -0600

    feat: auto-map data fields in eleventyComputed from external YAML configuration

--- _page.11tydata.js
+++ _page.11tydata.js
@@ -1,6 +1,16 @@
+import fs from "fs";
+import yaml from "js-yaml";
+
+// Load YAML directly using a relative string path
+const pages = yaml.load(fs.readFileSync("../_data/pages.yml", "utf8"));
+const allKeys = [...new Set(pages.flatMap((obj) => Object.keys(obj)))];
+console.log("Data fields to be auto-mapped:", allKeys);
+
 export default {
   eleventyComputed: {
-    eleventyNavigation: (data) => data._.eleventyNavigation,
-    iframe: (data) => data._.iframe,
+    ...allKeys.reduce((acc, key) => {
+      acc[key] = (data) => data._[key];
+      return acc;
+    }, {}),
   },
 };
