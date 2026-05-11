f81f56f6828ccbe7dac376ca83b3d6881a8bd224
Anton Staroverov <73071319+tonystar@users.noreply.github.com>
2026-05-11 00:00:08 -0600

    feat: refactor page layout to use template inheritance and support dynamic iframe rendering

--- _page.11tydata.js
+++ _page.11tydata.js
@@ -3,13 +3,15 @@ import yaml from "js-yaml";
 
 // Load YAML directly using a relative string path
 const pages = yaml.load(fs.readFileSync("../_data/pages.yml", "utf8"));
-const allKeys = [...new Set(pages.flatMap((obj) => Object.keys(obj)))];
+const allKeys = [...new Set(pages.flatMap((obj) => Object.keys(obj)))].filter(
+  (key) => key !== "permalink",
+);
 console.log("Data fields to be auto-mapped:", allKeys);
 
 export default {
   eleventyComputed: {
     ...allKeys.reduce((acc, key) => {
-      acc[key] = (data) => data._[key];
+      if (!(key in acc)) acc[key] = (data) => data._?.[key];
       return acc;
     }, {}),
   },
